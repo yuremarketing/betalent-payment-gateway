aeded9c0e02dc3e263399b48f3fe7ae02ea4e61d
yuremarketing <yure.mark@gmail.com>
2026-03-11 15:30:34 -0300

    feat: eu implementei a rota de listagem e o método index com paginação

--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,9 @@ use Illuminate\Support\Facades\Route;
 | Usei '::class' para que o Laravel consiga localizar o controlador corretamente.
 */
 Route::post('/payments', [PaymentController::class, 'store']);
+
+/*
+|criei o endpoint de listagem de transações para iniciar o Card 5
+|implementei a rota de listagem e o método index com paginação
+*/
+Route::get('/transactions', [App\Http\Controllers\PaymentController::class, 'index']);
